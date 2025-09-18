f9a583d202c36fdaba03b425b1a0c7a41b69e7d2
ze-fernando <zefernando.dev@gmail.com>
2025-09-18 20:29:19 -0300

    feat: chatbar

--- app/Home/styles.tsx
+++ app/Home/styles.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
     },
     boxValue: {
         color: "white",
-        fontSize: 15,
+        fontSize: 13,
         fontWeight: "bold",
     },
     pieBox: {
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
     },
     barChartSection: {
+        bottom: 20,
         marginBottom: 32,
     },
 });
